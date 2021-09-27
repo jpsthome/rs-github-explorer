fcc6624273545a41295b95c9d8d32f0154d83203
Joao Pedro <joao.thome@unila.edu.br>
2021-09-27 20:06:31 -0300

    adicionando useEffect para fazer chamada à API do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,26 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
+
 import '../styles/repositories.scss'
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
 	name: 'unform',
 	description: 'Forms in React',
 	link: 'https://github.com/unform/unform'
 };
-
 export function RepositoryList(){
+	const [repositories, setRepositories] = useState([]);
+
+
+
+	useEffect(()=> {
+		fetch('https://api.github.com/orgs/rocketseat/repos')
+		.then(response => response.json())
+		.then(data => setRepositories(data))
+	}, [])
+
 	return (
 		<section className="repository-list">
 			<h1>Lista de repositórios</h1>
